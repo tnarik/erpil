c29392ad82311daaf14dc898d10cffb32cc1418b
tnarik <tnarik@gmail.com>
2014-06-10 23:01:03 +0100

    Added staging configuration.

---
+++ app/config/staging/database.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| PDO Fetch Style
+	|--------------------------------------------------------------------------
+	|
+	| By default, database results will be returned as instances of the PHP
+	| stdClass object; however, you may desire to retrieve records in an
+	| array format for simplicity. Here you can tweak the fetch style.
+	|
+	*/
+
+	'fetch' => PDO::FETCH_CLASS,
+	'default' => 'sqlite',
+	'connections' => array(
+
+		'sqlite' => array(
+			'driver'   => 'sqlite',
+			'database' => __DIR__.'/../../database/staging.sqlite',
+			'prefix'   => '',
+		),
+	),
+);
